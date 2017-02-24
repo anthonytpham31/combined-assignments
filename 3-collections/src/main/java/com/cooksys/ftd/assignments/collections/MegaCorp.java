b53d1c62c21a39aab73f9e97fe57a1d57e34b21e
Anthony Pham <anthonytpham31@gmail.com>
2017-02-23 22:01:28 -0600

    Finished all 3 assignments; Will go into refining code and doing simplified rational over weekend

--- 3-collections/src/main/java/com/cooksys/ftd/assignments/collections/MegaCorp.java
+++ 3-collections/src/main/java/com/cooksys/ftd/assignments/collections/MegaCorp.java
@@ -48,6 +48,7 @@ public class MegaCorp implements Hierarchy<Capitalist, FatCat> {
     	}
     	if(capitalist.hasParent() && hierarchyMegaCorp.contains(capitalist.getParent())){
     		hierarchyMegaCorp.add(capitalist);
+    		return true;
     	}
     	if(capitalist.hasParent() == false && capitalist instanceof FatCat){
     		hierarchyMegaCorp.add(capitalist);
@@ -131,9 +132,13 @@ public class MegaCorp implements Hierarchy<Capitalist, FatCat> {
     @Override
     public Map<FatCat, Set<Capitalist>> getHierarchy() {
     	
-    	Map<FatCat, Set<Capitalist>> mapHierarchy = new HashMap<>();
-
+    	HashMap<FatCat, Set<Capitalist>> mapHierarchy = new HashMap<>();
     	
+    	for (Capitalist parent : hierarchyMegaCorp) {
+    		if(parent instanceof FatCat){
+    			mapHierarchy.put((FatCat) parent, getChildren((FatCat) parent));
+    		}
+		}
     	
     	return mapHierarchy;
     }
@@ -146,18 +151,32 @@ public class MegaCorp implements Hierarchy<Capitalist, FatCat> {
      */
     @Override
     public List<FatCat> getParentChain(Capitalist capitalist) {
-    	
+    	  
     	ArrayList<FatCat> parentList = new ArrayList<FatCat>();
     	
     	if(capitalist == null){
     		return parentList;
     	}
     	
-    	if(capitalist.hasParent()){
-    		while(capitalist.getParent() != null){
-    			parentList.add(capitalist.getParent());
+    	FatCat parent;
+    	parent = capitalist.getParent();
+    	
+    	
+    	while(parent != null){
+    		if(hierarchyMegaCorp.contains(parent)){
+    			parentList.add(parent);
+    			parent = parent.getParent();
+    		}
+    		else{
+    			parentList.clear();
+    			return parentList;
     		}
     	}
+    	
+    	if(capitalist.hasParent() == false){
+    		parentList.clear();
+    	}
+    	
     	
     	return parentList;
     }
